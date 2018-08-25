Small fixes for policy gradient classes.

vel/rl/modules/double_q_head.py
@@ -7,7 +7,7 @@ import gym.spaces as spaces
 
 
 class DoubleQHead(nn.Module):
-    """ Network head calculating Q-function value for each action. """
+    """ Network head calculating Q-function value for each (discrete) action using two separate inputs. """
     def __init__(self, input_dim, action_space):
         super().__init__()
 