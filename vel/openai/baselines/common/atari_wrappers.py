ee1958bed56d8e9bdb568936abbf04c57be78a7d
Million Integrals <jerry@millionintegrals.com>
2018-11-13 23:24:58 +0100

    Breakout env evaluation needs to fire on reset as well.

--- vel/openai/baselines/common/atari_wrappers.py
+++ vel/openai/baselines/common/atari_wrappers.py
@@ -121,7 +121,14 @@ class FireEpisodicLifeEnv(gym.Wrapper):
         return obs, reward, done, info
 
     def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
+        self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(1)
+        if done:
+            self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset(**kwargs)
+        return obs
 
 
 class MaxAndSkipEnv(gym.Wrapper):
