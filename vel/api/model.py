846fd4d1a10c6941864db558c9100e735d1fe574
Million Integrals <jerry@millionintegrals.com>
2019-04-08 22:34:39 -0700

    Reducing number of parameters.

--- vel/api/model.py
+++ vel/api/model.py
@@ -39,10 +39,10 @@ class Model(nn.Module):
 
         if input_size is None:
             print(self)
-            print("-" * 120)
+            print("-" * 100)
             number = sum(p.numel() for p in self.parameters())
             print("Number of model parameters: {:,}".format(number))
-            print("-" * 120)
+            print("-" * 100)
         else:
             summary(self, input_size)
 
