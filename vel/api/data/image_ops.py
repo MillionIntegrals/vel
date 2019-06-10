38e123146d10d4137107888d9087299fe140584c
Jerry Tworek <jerry@millionintegrals.com>
2019-06-09 20:00:11 -0700

    Redid formatting of image_ops.

--- vel/api/data/image_ops.py
+++ vel/api/data/image_ops.py
@@ -4,32 +4,28 @@ import numpy as np
 
 
 def crop_square(im, r, c, sz):
-    '''
-    crop image into a square of size sz,
-    '''
-    return im[r:r+sz, c:c+sz]
+    """ Crop image into a square of size sz. """
+    return im[r:r + sz, c:c + sz]
 
 
 def crop(im, r, c, sz_h, sz_w):
-    '''
-    crop image into a square of size sz,
-    '''
-    return im[r:r+sz_h, c:c+sz_w]
+    """ Crop image into a of size sz_w x sz_h. """
+    return im[r:r + sz_h, c:c + sz_w]
 
 
 def center_crop(im, min_sz=None):
     """ Returns a center crop of an image"""
     # return F.center_crop(im, min_sz)
-    r,c,*_ = im.shape
-    if min_sz is None: min_sz = min(r,c)
-    start_r = math.ceil((r-min_sz)/2)
-    start_c = math.ceil((c-min_sz)/2)
+    r, c, *_ = im.shape
+    if min_sz is None: min_sz = min(r, c)
+    start_r = math.ceil((r - min_sz) / 2)
+    start_c = math.ceil((c - min_sz) / 2)
     return crop_square(im, start_r, start_c, min_sz)
 
 
 def scale_to(x, ratio, targ):
-    '''Calculate dimension of an image during scaling with aspect ratio'''
-    return max(math.floor(x*ratio), targ)
+    """ Calculate dimension of an image during scaling with aspect ratio """
+    return max(math.floor(x * ratio), targ)
 
 
 def scale_min(im, targ, interpolation=cv2.INTER_AREA):
@@ -39,9 +35,9 @@ def scale_min(im, targ, interpolation=cv2.INTER_AREA):
         im (array): image
         targ (int): target size
     """
-    r,c,*_ = im.shape
+    r, c, *_ = im.shape
 
-    ratio = targ/min(r,c)
+    ratio = targ / min(r, c)
 
     sz = (scale_to(c, ratio, targ), scale_to(r, ratio, targ))
 
@@ -59,9 +55,12 @@ def rotate_img(im, deg, mode=cv2.BORDER_CONSTANT, interpolation=cv2.INTER_AREA):
     Arguments:
         deg (float): degree to rotate.
     """
-    r,c,*_ = im.shape
-    M = cv2.getRotationMatrix2D((c//2,r//2),deg,1)
-    return cv2.warpAffine(im,M,(c,r), borderMode=mode, flags=cv2.WARP_FILL_OUTLIERS+interpolation)
+    r, c, *_ = im.shape
+    M = cv2.getRotationMatrix2D((c // 2, r // 2), deg, 1)
+    return cv2.warpAffine(
+        im, M, (c, r), borderMode=mode,
+        flags=cv2.WARP_FILL_OUTLIERS + interpolation
+    )
 
 
 def pad(img, pad, mode=cv2.BORDER_REFLECT):
@@ -78,7 +77,7 @@ def mode_to_cv2(mode='constant'):
 
 
 def lighting(im, b, c):
-    ''' adjusts image's balance and contrast'''
-    if b==0 and c==1: return im
+    """ Adjusts image's balance and contrast. """
+    if b == 0 and c == 1: return im
     mu = np.average(im)
-    return np.clip((im-mu)*c+mu+b,0.,1.).astype(np.float32)
+    return np.clip((im - mu) * c + mu + b, 0., 1.).astype(np.float32)
