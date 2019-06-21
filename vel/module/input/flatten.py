30e654e5c8dc99553bb3b014932f268371b933b6
Jerry Tworek <jerry@millionintegrals.com>
2019-06-20 17:30:46 -0700

    Added a few extra input modules.

---
+++ vel/module/input/flatten.py
@@ -0,0 +1,28 @@
+from vel.module.layers import Flatten
+
+
+from vel.api import ModelFactory, BackboneModel
+
+
+class FlattenInput(BackboneModel):
+    """ Sequence input """
+    def __init__(self):
+        super().__init__()
+        self.model = Flatten()
+
+    def forward(self, input_data):
+        return self.model(input_data)
+
+
+def create():
+    """ Vel factory function """
+    def instantiate(**_):
+        return Flatten()
+
+    return ModelFactory.generic(instantiate)
+
+
+# Scripting interface
+FlattenInputFactory = create
+
+
