284260a115287a92b7b3270aba4bdf1248d75b9c
Million Integrals <jerry@millionintegrals.com>
2019-10-14 12:05:46 -0700

    EWMA input normalization.

---
+++ vel/module/input/normalize_ewma.py
@@ -0,0 +1,47 @@
+import torch
+
+from vel.api import VModule
+
+
+class NormalizeEwma(VModule):
+    """ Normalize a vector of observations - across the batch dim """
+
+    def __init__(self, input_shape, beta=0.99, per_element_update=False, epsilon=1e-1):
+        super().__init__()
+
+        self.input_shape = input_shape
+        self.epsilon = epsilon
+        self.beta = beta
+        self.per_element_update = per_element_update
+
+        self.register_buffer('running_mean', torch.zeros(input_shape, dtype=torch.float))
+        self.register_buffer('running_var', torch.ones(input_shape, dtype=torch.float))
+        self.register_buffer('debiasing_term', torch.tensor(self.epsilon, dtype=torch.float))
+
+    def reset_weights(self):
+        self.running_mean.zero_()
+        self.running_var.fill_(1.0)
+        self.count.fill_(self.epsilon)
+
+    def forward(self, input_vector):
+        # Make sure input is float32
+        input_vector = input_vector.to(torch.float)
+
+        if self.training:
+            batch_size = input_vector.size(0)
+            batch_mean = input_vector.mean(dim=0)
+            batch_var = input_vector.var(dim=0, unbiased=False)
+
+            if self.per_element_update:
+                weight = self.beta ** batch_size
+            else:
+                weight = self.beta
+
+            self.running_mean.mul_(weight).add_(batch_mean * (1.0 - weight))
+            self.running_var.mul_(weight).add_(batch_var * (1.0 - weight))
+            self.debiasing_term.mul_(weight).add_(1.0 * (1.0 - weight))
+
+        debiased_mean = self.running_mean / self.debiasing_term
+        debiased_var = self.running_var / self.debiasing_term
+
+        return (input_vector - debiased_mean.unsqueeze(0)) / torch.sqrt(debiased_var.unsqueeze(0))
---
+++ vel/net/layer/input/normalize_ewma.py
@@ -0,0 +1,65 @@
+from vel.api import SizeHints, SizeHint
+from vel.module.input.normalize_ewma import NormalizeEwma
+from vel.net.layer_base import LayerFactory, Layer, LayerFactoryContext, LayerInfo
+
+
+class NormalizeEwmaLayer(Layer):
+    """ Layer that normalizes the inputs """
+
+    def __init__(self, info: LayerInfo, input_shape: SizeHints, beta: float = 0.99, epsilon: float = 1e-1,
+                 per_element_update=False):
+        super().__init__(info)
+
+        self.input_shape = input_shape
+        self.beta = beta
+        self.epsilon = epsilon
+        self.per_element_update = per_element_update
+
+        self.normalize = NormalizeEwma(
+            beta=self.beta,
+            epsilon=self.epsilon,
+            per_element_update=self.per_element_update,
+            input_shape=self.input_shape.assert_single()[1:]  # Remove batch axis
+        )
+
+    def forward(self, direct, state: dict = None, context: dict = None):
+        return self.normalize(direct)
+
+    def size_hints(self) -> SizeHints:
+        return self.input_shape
+
+
+class NormalizeEwmaLayerFactory(LayerFactory):
+    def __init__(self, beta: float = 0.99, epsilon: float = 1e-2, shape=None, per_element_update=False):
+        super().__init__()
+        self.shape = shape
+        self.beta = beta
+        self.epsilon = epsilon
+        self.per_element_update = per_element_update
+
+    @property
+    def name_base(self) -> str:
+        """ Base of layer name """
+        return "image_to_tensor"
+
+    def instantiate(self, direct_input: SizeHints, context: LayerFactoryContext, extra_args: dict) -> Layer:
+        """ Create a given layer object """
+        if self.shape is None:
+            input_shape = direct_input
+        else:
+            input_shape = SizeHints(SizeHint(*([None] + list(self.shape))))
+
+        return NormalizeEwmaLayer(
+            info=self.make_info(context),
+            beta=self.beta,
+            epsilon=self.epsilon,
+            per_element_update=self.per_element_update,
+            input_shape=input_shape
+        )
+
+
+def create(beta=0.99, epsilon=1e-1, shape=None, per_element_update=False, label=None, group=None):
+    """ Vel factory function """
+    return NormalizeEwmaLayerFactory(
+        beta=beta, epsilon=epsilon, shape=shape, per_element_update=per_element_update
+    ).with_given_name(label).with_given_group(group)
