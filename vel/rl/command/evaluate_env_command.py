4636bba3f5e95afbd608107466166d80b3a5be85
Million Integrals <jerry@millionintegrals.com>
2019-11-09 12:05:36 -0800

    Fixed evaluation command.

--- vel/rl/command/evaluate_env_command.py
+++ vel/rl/command/evaluate_env_command.py
@@ -2,24 +2,22 @@ import numpy as np
 import pandas as pd
 import torch
 import tqdm
-import typing
 
 from vel.api import ModelConfig, TrainingInfo, Storage, ModuleFactory
 from vel.rl.api import VecEnvFactory
+from vel.rl.util.actor import PolicyActor
 
 
 class EvaluateEnvCommand:
     """ Record environment playthrough as a game  """
     def __init__(self, model_config: ModelConfig, env_factory: VecEnvFactory, model_factory: ModuleFactory,
-                 storage: Storage, parallel_envs: int, action_noise: typing.Optional[ModuleFactory], takes: int,
-                 sample_args: dict = None):
+                 storage: Storage, parallel_envs: int, takes: int, sample_args: dict = None):
         self.model_config = model_config
         self.model_factory = model_factory
         self.env_factory = env_factory
         self.storage = storage
         self.takes = takes
         self.parallel_envs = parallel_envs
-        self.action_noise_factory = action_noise
 
         self.sample_args = sample_args if sample_args is not None else {}
 
@@ -30,12 +28,9 @@ class EvaluateEnvCommand:
         env = self.env_factory.instantiate(
             parallel_envs=self.parallel_envs, preset='record', seed=self.model_config.seed
         )
-        model = self.model_factory.instantiate(action_space=env.action_space).to(device)
-
-        if self.action_noise_factory is not None:
-            action_noise = self.action_noise_factory.instantiate(environment=env).to(device)
-        else:
-            action_noise = None
+        model = self.model_factory.instantiate(
+            action_space=env.action_space, observation_space=env.observation_space
+        ).to(device)
 
         training_info = TrainingInfo(
             start_epoch_idx=self.storage.last_epoch_idx()
@@ -48,26 +43,17 @@ class EvaluateEnvCommand:
 
         model.eval()
 
+        actor = PolicyActor(num_envs=self.parallel_envs, policy=model, device=device)
+
         episode_rewards = []
         episode_lengths = []
 
         observations = env.reset()
         observations_tensor = torch.from_numpy(observations).to(device)
 
-        if model.is_stateful:
-            hidden_state = model.zero_state(observations.shape[0]).to(device)
-
         with tqdm.tqdm(total=self.takes) as progress_bar:
             while len(episode_rewards) < self.takes:
-                if model.is_stateful:
-                    output = model.step(observations_tensor, hidden_state, **self.sample_args)
-                    hidden_state = output['state']
-                    actions = output['actions']
-                else:
-                    actions = model.step(observations_tensor, **self.sample_args)['actions']
-
-                if action_noise is not None:
-                    actions = action_noise(actions)
+                actions = actor.act(observations_tensor, **self.sample_args)['actions']
 
                 observations, rewards, dones, infos = env.step(actions.cpu().numpy())
                 observations_tensor = torch.from_numpy(observations).to(device)
@@ -78,22 +64,19 @@ class EvaluateEnvCommand:
                         episode_lengths.append(info['episode']['l'])
                         progress_bar.update(1)
 
-                if model.is_stateful:
-                    # Zero state belongiong to finished episodes
-                    dones_tensor = torch.from_numpy(dones.astype(np.float32)).to(device)
-                    hidden_state = hidden_state * (1.0 - dones_tensor.unsqueeze(-1))
+                dones_tensor = torch.from_numpy(dones.astype(np.float32)).to(device)
+                actor.reset_states(dones_tensor)
 
         print(pd.DataFrame({'lengths': episode_lengths, 'rewards': episode_rewards}).describe())
 
 
-def create(model_config, model, vec_env, storage, takes, parallel_envs, action_noise=None, sample_args=None):
+def create(model_config, model, vec_env, storage, takes, parallel_envs, sample_args=None):
     """ Vel factory function """
     return EvaluateEnvCommand(
         model_config=model_config,
         model_factory=model,
         env_factory=vec_env,
         parallel_envs=parallel_envs,
-        action_noise=action_noise,
         storage=storage,
         takes=takes,
         sample_args=sample_args
